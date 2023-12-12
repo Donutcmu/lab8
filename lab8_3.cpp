1861260da9170d597cabd9047fd4a4cadd2d62a5
Donut <punnatat_ng@cmu.ac.th>
2023-12-12 15:42:42 +0700

    good

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,53 @@
 "Brook"
 "Jinbe"
 */
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int age,height,bounty;
+    cout<<"Enter your age: ";
+    cin>>age;
+    /*cout<<"Enter your height: ";
+    cin>>height;
+    cout<<"Enter your bounty: ";
+    cin>>bounty;*/
+    if(age<=25){
+        cout<<"Enter your height: ";
+        cin>>height;
+        if(height<100){
+            cout<<"Your character = Chopper";
+            //return 0;
+        }
+        else if(height<180){
+            cout<<"Your character = Usopp";
+            //return 0;
+        }
+        else{
+            cout<<"Enter your bounty: ";
+            cin>>bounty;
+            if(bounty<=1100000000){
+                cout<<"Your character = Sanji";
+            }
+            else{
+                cout<<"Your character = Zoro";
+            }
+        }
+
+    }
+    else if(age<=60){
+        cout<<"Enter your bounty: ";
+        cin>>bounty;
+        if(bounty>500000000){
+            cout<<"Your character = Jinbe";
+        }
+        else{
+            cout<<"Your character = Franky";
+        }
+    }
+    else{
+        cout<<"Your character = Brook";
+    }
+    return 0;
+}
